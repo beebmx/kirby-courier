cd1833999280a75fffa5a4489c69a32f61163061
Fernando Gutierrez <fernando@beeb.mx>
2025-06-19 15:31:45 -0600

    1.0.6
    - Added default options
    - Update pest file
    - Removed kirby directory from root package

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use Kirby\Cms\App as Kirby;
 Kirby::plugin('beebmx/kirby-courier', [
     'commands' => require_once __DIR__.'/extensions/commands.php',
     'hooks' => require_once __DIR__.'/extensions/hooks.php',
+    'options' => require_once __DIR__.'/extensions/options.php',
     'snippets' => require_once __DIR__.'/extensions/snippets.php',
     'templates' => require_once __DIR__.'/extensions/templates.php',
 ]);
